87fed5b6dd761a112d6774fcbfcecf0ee7850237
ktpi2000 <katapi2000@gmail.com>
2023-02-24 23:39:46 +0900

    add search component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Container, Space, Text } from '@mantine/core';
+import { Container, Input, Space } from '@mantine/core';
+import { useState } from 'react';
 import { LineItem } from '../components/lineItem/LineItem';
 
 const list = [
@@ -15,10 +16,19 @@ const list = [
 ];
 
 export default function HomePage() {
+  const [keyword, setKeyword] = useState('');
+
+  const filteredList = list.filter((line) => new RegExp(keyword).test(line.name));
+
   return (
     <Container size="xs">
-      <Text>JREAST TIMETABLE</Text>
-      {list.map((item, index) => (
+      <Input
+        value={keyword}
+        onChange={(event) => setKeyword(event.currentTarget.value)}
+        placeholder="路線名で絞り込み"
+      />
+
+      {filteredList.map((item, index) => (
         <div>
           <Space h="sm" />
           <LineItem key={index} name={item.name} weekdays={item.weekdays} holiday={item.holiday} />
